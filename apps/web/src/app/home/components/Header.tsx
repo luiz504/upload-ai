7c977f9d3f268e006bb721e7fb4f397c69ddb5a6
Luiz Bueno <luiz504@hotmail.com>
2023-09-14 21:15:05 -0300

    feat: main page layout creation

---
+++ apps/web/src/app/home/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Github } from 'lucide-react'
+import { FC } from 'react'
+import { Button } from '~/components/ui/button'
+import { Separator } from '~/components/ui/separator'
+
+// import { Container } from './styles';
+
+export const Header: FC = () => {
+  return (
+    <header className="flex items-center justify-between border-b px-6 py-3">
+      <h1 className="text-xl font-bold">upload.ai</h1>
+
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">Developed with 💜</span>
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <Button variant="outline" className="gap-2">
+          <Github className="w-4 h-4" /> Github
+        </Button>
+      </div>
+    </header>
+  )
+}
