03ac485712e03de819175bf44e724d043beb56cc
Luiz Bueno <luiz504@hotmail.com>
2023-09-16 15:30:25 -0300

    backend(routes): upload-video create-transcription

---
+++ apps/backend/src/lib/openai.ts
@@ -0,0 +1,4 @@
+import 'dotenv/config'
+import OpenAI from 'openai'
+
+export const openAI = new OpenAI({ apiKey: process.env.OPENAI_KEY })
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,10 +1,14 @@
 import { fastify } from 'fastify'
 
 import { getAllPrompts } from '~/routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscription } from './routes/create-transcription'
 
 const app = fastify()
 
 app.register(getAllPrompts)
+app.register(uploadVideoRoute)
+app.register(createTranscription)
 
 app
   .listen({ port: 3333 })
